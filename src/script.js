5c0b86e86a1e391ae62feecfeaf9dfcb0b59b6d8
khalid <student3045612@gmail.com>
2025-04-26 19:34:57 +0530

    modify swiper script

--- src/script.js
+++ src/script.js
@@ -62,10 +62,10 @@ var swiper = new Swiper(".eduleSwiper", {
   slidesPerView: 5,
   spaceBetween: 30,
   loop:true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   breakpoints: {
     1200: {
       slidesPerView: 5, // large screens (tablets, etc.)
